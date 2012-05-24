1cf1e4871a7c8773330c3e18837b1c81501c96e8
Richard Trip <richard.trip@gmail.com>
2012-05-24 19:11:49 +0200

    Revert VDD safe step to 100mV

--- arch/arm/mach-tegra/tegra3_dvfs.c
+++ arch/arm/mach-tegra/tegra3_dvfs.c
@@ -45,7 +45,7 @@ static const int core_millivolts[MAX_DVFS_FREQS] = {
 #define VDD_CPU_BELOW_VDD_CORE		300
 static int cpu_below_core = VDD_CPU_BELOW_VDD_CORE;
 
-#define VDD_SAFE_STEP			50
+#define VDD_SAFE_STEP			100
 
 static struct dvfs_rail tegra3_dvfs_rail_vdd_cpu = {
 	.reg_id = "vdd_cpu",
